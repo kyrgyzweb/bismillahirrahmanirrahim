cb32f576ce0d4e9632ac68484063bee1baa87a15
Abdullah Mamazhusup <newabdul2020@gmail.com>
2024-03-15 11:03:51 +0600

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toggle, setToggle] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>The toggle is {toggle}</p>
+      <button onClick={() => setToggle('On')}>On</button>
+      <button onClick={() => setToggle('Off')}>Off</button>
     </div>
-  );
+  )
 }
 
 export default App;
